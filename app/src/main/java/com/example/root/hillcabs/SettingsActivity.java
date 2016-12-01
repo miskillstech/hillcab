9a95a0d6f0c205da0d174e5125558470712c0e2f
miskilltech@gmail.com <miskilltech@gmail.com>
2016-12-01 20:05:48 +0530

    logout done

--- app/src/main/java/com/example/root/hillcabs/SettingsActivity.java
+++ app/src/main/java/com/example/root/hillcabs/SettingsActivity.java
@@ -1,24 +1,34 @@
 package com.example.root.hillcabs;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.nfc.Tag;
+import android.os.Handler;
 import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener{
 
     ImageView ivback;
     TextView tvlogout;
+    private SessionManager session;
     private EditText input_first_name,input_last_name, inputEmail, inputPassword;
     private TextInputLayout input_layout_first_name,input_layout_last_name, inputLayoutEmail, inputLayoutPassword;
 
@@ -28,13 +38,11 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         ivback = (ImageView) findViewById(R.id.ivback);
+        tvlogout = (TextView) findViewById(R.id.tvlogout);
 
-        ivback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+        ivback.setOnClickListener(this);
+
+        tvlogout.setOnClickListener(this);
 
         input_layout_first_name = (TextInputLayout) findViewById(R.id.input_layout_first_name);
         input_layout_last_name = (TextInputLayout) findViewById(R.id.input_layout_last_name);
@@ -44,7 +52,7 @@ public class SettingsActivity extends AppCompatActivity {
         input_last_name = (EditText) findViewById(R.id.input_last_name);
         inputEmail = (EditText) findViewById(R.id.input_email);
         inputPassword = (EditText) findViewById(R.id.input_mobile);
-        tvlogout = (TextView) findViewById(R.id.tvlogout);
+
 
         input_first_name.addTextChangedListener(new MyTextWatcher(input_first_name));
         input_last_name.addTextChangedListener(new MyTextWatcher(input_last_name));
@@ -132,6 +140,38 @@ public class SettingsActivity extends AppCompatActivity {
     private void requestFocus(View view) {
         if (view.requestFocus()) {
             getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+
+    }
+
+    private static String TAG = SessionManager.class.getSimpleName();
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ivback:
+
+                break;
+
+            case R.id.tvlogout:
+
+
+
+                SharedPreferences myPrefs = getSharedPreferences("Activity",
+                        MODE_PRIVATE);
+                SharedPreferences.Editor editor = myPrefs.edit();
+                editor.clear();
+                editor.commit();
+                //AppState.getSingleInstance().setLoggingOut(true);
+                session.setLogin(true);
+                Log.d(TAG, "Now log out and start the activity login");
+
+                Intent intent = new Intent(SettingsActivity. this,
+                    LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+                break;
         }
     }
 
